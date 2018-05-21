067d5834b72d6fc4d2cb0612d0ac315a49aa3366
Farid <36966275+Farid-4K@users.noreply.github.com>
2018-05-21 15:52:56 +0700

    Update Db.php

--- application/lib/Db.php
+++ application/lib/Db.php
@@ -8,7 +8,6 @@ class Db extends R
 	public function __construct()
 	{
 		R::setup( 'mysql:host=localhost;dbname=blog', 'root', '123' );
-		// R::setup( 'mysql:host=localhost;dbname=id4768278_goldenhands', 'id4768278_farid5ip50gmailcom', '1938666farid' );
 		if ( !R::testConnection() )
 			{
 				exit ('Нет соединения с базой данных');
