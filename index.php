fa8b22d6407797bad81628e0fcff0d2df5878089
Farid-4K <farid5ip50@gmail.com>
2018-05-21 22:53:47 +0700

    fix

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-/* constants */
+// constants
 define('ROOT', dirname(__FILE__));
 define('SITENAME', "http://h0stname.tk");
 
