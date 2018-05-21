9002682845d908f97c8adef7c066f82941925b02
Farid-4K <farid5ip50@gmail.com>
2018-05-21 21:09:24 +0700

    Fixed

--- index.php
+++ index.php
@@ -3,7 +3,11 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+/* constants */
 define('ROOT', dirname(__FILE__));
+define('SITENAME', "http://h0stname.tk");
+
 session_start();
 
 require_once 'application/lib/dev.php';
