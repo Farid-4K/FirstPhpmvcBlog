bb421b3ec8021d9ef7c8241037a6375390d79d85
Farid-4K <farid5ip50@gmail.com>
2018-05-21 21:37:29 +0700

    fix db

--- application/lib/Db.php
+++ application/lib/Db.php
@@ -1,13 +1,22 @@
 <?php
 namespace application\lib;
 
-require_once ROOT."/application/lib/rb.php";
+require_once ROOT . "/application/lib/rb.php";
 use R;
+
 class Db extends R
 {
-	public function __construct()
-	{
-		R::setup( 'mysql:host=localhost;dbname=goldenhands', 'root', '123' );
-		// R::setup( 'mysql:host=localhost;dbname=id4768278_goldenhands', 'id4768278_farid5ip50gmailcom', '1938666farid' );
-	}
+   public function __construct()
+   {
+      /* Database setting */
+      $db_host = "localhost";
+      $db_name = "blog";
+      $db_user = "root";
+      $db_password = "123";
+
+      R::setup('mysql:host=' . $db_host . ';dbname=' . $db_name, $db_user, $db_password);
+      if (!R::testConnection()) {
+         exit('Нет соединения с базой данных');
+      }
+   }
 }
