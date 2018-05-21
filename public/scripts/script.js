1500ed2fc37c063318dbaa7b0bdc1cb86a588200
Farid <36966275+Farid-4K@users.noreply.github.com>
2018-05-21 21:15:41 +0700

    fixed

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -10,6 +10,24 @@ $(document).ready(function (){
 		$(".close-menu").toggleClass("menu close-menu");
 	});
 
+	$("#open_register").click(function (){
+		$("#register").fadeIn(300, "swing");
+		$("#login").fadeOut(200, "swing");
+		$(this).addClass("filter-grey");
+	});
+
+	$("#open_login").click(function (){
+		$("#register").fadeOut(200, "swing");
+		$("#login").fadeIn(300, "swing");
+		$(this).addClass("filter-grey");
+	});
+
+	$(".id-exit-form").click(function (){
+		$(this).parent().parent().fadeOut(300, "swing");
+		$("#open_register").removeClass("filter-grey");
+		$("#open_login").removeClass("filter-grey");
+	});
+
 	$(".userInput").focus(function (){
 		$(this).parent().addClass("focus");
 	}).blur(function (){
@@ -18,8 +36,13 @@ $(document).ready(function (){
 		}
 	});
 
+
 	$(".btn-card-exit").click(function (){
-		$(this).parent().fadeOut();
+		$(this).parent().animate({
+			opacity: 0,
+		}, 310, "swing", function (){
+			$(this).toggle();
+		});
 	});
 
 	$(".notification").click(function (){
