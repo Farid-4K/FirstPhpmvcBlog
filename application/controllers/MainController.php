8b3996f2333d511304a56095953ed5f0fb1fe0fe
Farid-4K <farid5ip50@gmail.com>
2018-05-21 21:41:10 +0700

    Fixed

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -2,46 +2,44 @@
 
 namespace application\controllers;
 
-use application\lib\Db;
 use application\core\Controller;
 
 class MainController extends Controller
 {
-	public function indexAction()
-	{
-		// Content for page
-		$vars = [
-			'buttons' => [
-				['Категории','#','category'],
-				['Поиск','#','search'],
-				['Мой магазин','/members/market',''],
-				['Регистрация','/account/registration', ''],
-				['Авторизация','/account/login', '']
-			],
-		];
+   public function indexAction()
+   {
+      // Content for page
+      $vars = [
+         'buttons' => [
+            ['Категории', '#', 'category'],
+            ['Поиск', '#', 'search'],
+            ['Мой блог', '/members/blog', ''],
+            ['Регистрация', '/account/registration', ''],
+            ['Авторизация', '/account/login', ''],
+         ],
+      ];
 
-		if(!empty($_REQUEST['name'])){
-			exit(json_encode(['status' => $_REQUEST, 'message' => 'message']));
-		}
-		// Render page
-		$this->view->render('Golden Hands', $vars);
-	}
+      if (!empty($_REQUEST['name'])) {
+         exit(json_encode(['status' => $_REQUEST, 'message' => 'message']));
+      }
+      // Render page
+      $this->view->render('Blog', $vars);
+   }
 
-	public function ajaxpreviewpostAction()
-	{
-		$this->view->layout = 'ajaxPreviewPost';
+   public function ajaxpreviewpostAction()
+   {
+      $this->view->layout = 'ajaxPreviewPost';
 
-		if (!empty($_REQUEST)) {
-			$find = $this->model->getPost($_REQUEST['card']);
-			$img_url = json_decode($find->img_url);
-			$vars = [
-				'name' => $find-> name,
-				'inform' => $find-> inform,
-				'category' => $find-> category,
-				'img_url' => $img_url,
-			];
-		}
-
-		$this->view->render('Golden Hands', $vars);
-	}
+      if (!empty($_REQUEST)) {
+         $find = $this->model->getPost($_REQUEST['card']);
+         $vars = [
+            'id'     => htmlentities($find->id),
+            'name'   => htmlentities($find->name),
+            'inform' => htmlentities($find->inform),
+            'image'  => htmlentities($find->image),
+            'date'   => htmlentities($find->date),
+         ];
+      }
+      $this->view->render('Blog', $vars);
+   }
 }
--- application/models/Main.php
+++ application/models/Main.php
@@ -4,11 +4,13 @@ namespace application\models;
 
 use application\core\Model;
 
-class Main extends Model {
+class Main extends Model
+{
 
-	public function getPost($id) {
-		$find = $this->db->findOne('markets', ' id = ? ', [$id]);
-		return $find;
-	}
+   public function getPost($id)
+   {
+      $find = $this->db->findOne('posts', ' id = ? ', [$id]);
+      return $find;
+   }
 
 }
